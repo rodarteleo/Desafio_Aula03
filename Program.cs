5a018afc9a0b817a087adb4345433777b8b89c06
rodarteleo <leo.rodarte@gmail.com>
2026-07-15 19:49:35 -0300

    2nd commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// criar um simulador de login com tentativas limitadas
+int maxTentativas = 3;
+int tentativas = 0;
+
+while (tentativas < maxTentativas)
+{
+    Console.Write("Digite seu usuário: ");
+    string usuario = Console.ReadLine();
+
+    Console.Write("Digite sua senha: ");
+    string senha = Console.ReadLine();
+
+    // Simulação de verificação de login
+    if (usuario == "admin" && senha == "1234")
+    {
+        Console.WriteLine("Login bem-sucedido!");
+        break;
+    }
+    else
+    {
+        tentativas++;
+        Console.WriteLine($"Usuário ou senha incorretos. Tentativa {tentativas} de {maxTentativas}.");
+        
+        if (tentativas == maxTentativas)
+        {
+            Console.WriteLine("Número máximo de tentativas atingido. Acesso negado.");
+        }
+    }
+}
